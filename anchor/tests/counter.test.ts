2b6b27c9b8a5915ddd7d9c21ebc546cb7651db61
saatvik-10 <saatvikmadan529@gmail.com>
2025-09-09 19:30:54 +0530

    added candidate test

--- anchor/tests/counter.test.ts
+++ anchor/tests/counter.test.ts
@@ -9,19 +9,68 @@ const IDL = require('../target/idl/voting.json')
 const votingAddress = new PublicKey('FqzkXZdwYjurnUKetJCAvaUw5WAqbwzU6gZEwydeEfqS')
 
 describe('Voting', () => {
-  it('Initialize Poll', async () => {
-    const context = await startAnchor('', [{ name: 'voting', programId: votingAddress }], [])
-    const provider = new BankrunProvider(context)
+  let context
+  let provider
+  let votingProgram
 
-    const votingProgram = new Program<Voting>(IDL, provider)
+  beforeAll(async () => {
+    context = await startAnchor('', [{ name: 'voting', programId: votingAddress }], [])
+    provider = new BankrunProvider(context)
+
+    votingProgram = new Program<Voting>(IDL, provider)
+  })
+
+  it('Initialize Poll', async () => {
+    const pollId = new anchor.BN(1)
 
     await votingProgram.methods
       .initializePoll(
-        new anchor.BN(1), //method to do u64 in ts
-        'MESSI or RONALDO?',
+        pollId, //method to do u64 in ts
+        'Who is the GOAT?',
         new anchor.BN(0),
         new anchor.BN(1857402735),
       )
       .rpc()
+
+    const [pollAddress] = PublicKey.findProgramAddressSync(
+      [pollId.toArrayLike(Buffer, 'le', 8)],
+      votingAddress,
+    )
+
+    const poll = await votingProgram.account.poll.fetch(pollAddress)
+
+    console.log(poll)
+
+    expect(poll.pollId.toNumber()).toEqual(1)
+    expect(poll.description).toEqual('MESSI or RONALDO?')
+    expect(poll.pollStart.toNumber()).toBeLessThan(poll.pollEnd.toNumber())
   })
+
+  it('Initialize Candidate', async () => {
+    const playerId = new anchor.BN(1);
+
+    await votingProgram.methods.initializeCandidate('MESSI', playerId).rpc()
+
+    const [messiAddress] = PublicKey.findProgramAddressSync(
+      [playerId.toArrayLike(Buffer, 'le', 8), Buffer.from('MESSI')],
+      votingAddress,
+    )
+
+    const messiPlayer = await votingProgram.account.candidate.fetch(messiAddress)
+
+    await votingProgram.methods.initializeCandidate('RONALDO', playerId).rpc()
+
+    const [ronaldoAddress] = PublicKey.findProgramAddressSync(
+      [playerId.toArrayLike(Buffer, 'le', 8), Buffer.from('MESSI')],
+      votingAddress,
+    )
+
+    const ronaldoPlayer = await votingProgram.account.candidate.fetch(ronaldoAddress)
+
+    console.log(messiPlayer)
+    console.log(ronaldoPlayer)
+  })
+
+  it('vote', async () => {})
 })
+
