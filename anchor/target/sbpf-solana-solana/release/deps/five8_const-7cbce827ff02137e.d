9571868ae6ad42da76d7a3f15cbb6235f725aad1
Saatvik__10 <146160585+saatvik-10@users.noreply.github.com>
2025-09-09 19:39:55 +0530

    Delete anchor/target directory

--- anchor/target/sbpf-solana-solana/release/deps/five8_const-7cbce827ff02137e.d
+++
@@ -1,8 +0,0 @@
-/home/pho3nix/Projects/voting-dapp/anchor/target/sbpf-solana-solana/release/deps/libfive8_const-7cbce827ff02137e.rmeta: /home/pho3nix/.cargo/registry/src/index.crates.io-6f17d22bba15001f/five8_const-0.1.4/src/lib.rs /home/pho3nix/.cargo/registry/src/index.crates.io-6f17d22bba15001f/five8_const-0.1.4/src/../README.md
-
-/home/pho3nix/Projects/voting-dapp/anchor/target/sbpf-solana-solana/release/deps/libfive8_const-7cbce827ff02137e.rlib: /home/pho3nix/.cargo/registry/src/index.crates.io-6f17d22bba15001f/five8_const-0.1.4/src/lib.rs /home/pho3nix/.cargo/registry/src/index.crates.io-6f17d22bba15001f/five8_const-0.1.4/src/../README.md
-
-/home/pho3nix/Projects/voting-dapp/anchor/target/sbpf-solana-solana/release/deps/five8_const-7cbce827ff02137e.d: /home/pho3nix/.cargo/registry/src/index.crates.io-6f17d22bba15001f/five8_const-0.1.4/src/lib.rs /home/pho3nix/.cargo/registry/src/index.crates.io-6f17d22bba15001f/five8_const-0.1.4/src/../README.md
-
-/home/pho3nix/.cargo/registry/src/index.crates.io-6f17d22bba15001f/five8_const-0.1.4/src/lib.rs:
-/home/pho3nix/.cargo/registry/src/index.crates.io-6f17d22bba15001f/five8_const-0.1.4/src/../README.md:
--- anchor/target/sbpf-solana-solana/release/deps/solana_atomic_u64-e227dfdef5f3548c.d
+++
@@ -1,7 +0,0 @@
-/home/pho3nix/Projects/voting-dapp/anchor/target/sbpf-solana-solana/release/deps/libsolana_atomic_u64-e227dfdef5f3548c.rmeta: /home/pho3nix/.cargo/registry/src/index.crates.io-6f17d22bba15001f/solana-atomic-u64-2.2.1/src/lib.rs
-
-/home/pho3nix/Projects/voting-dapp/anchor/target/sbpf-solana-solana/release/deps/libsolana_atomic_u64-e227dfdef5f3548c.rlib: /home/pho3nix/.cargo/registry/src/index.crates.io-6f17d22bba15001f/solana-atomic-u64-2.2.1/src/lib.rs
-
-/home/pho3nix/Projects/voting-dapp/anchor/target/sbpf-solana-solana/release/deps/solana_atomic_u64-e227dfdef5f3548c.d: /home/pho3nix/.cargo/registry/src/index.crates.io-6f17d22bba15001f/solana-atomic-u64-2.2.1/src/lib.rs
-
-/home/pho3nix/.cargo/registry/src/index.crates.io-6f17d22bba15001f/solana-atomic-u64-2.2.1/src/lib.rs:
