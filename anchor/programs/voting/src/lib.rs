7075f18edff0f47b3ecd3f452dafa14be86222c1
saatvik-10 <saatvikmadan529@gmail.com>
2025-09-09 12:31:43 +0530

    updated initialize_poll fn

--- anchor/programs/voting/src/lib.rs
+++ anchor/programs/voting/src/lib.rs
@@ -10,7 +10,20 @@ pub const ANCHOR_DISCRIMINATOR_SIZE: usize = 8;
 pub mod voting {
     use super::*;
 
-    pub fn initialize_poll(_ctx: Context<InitializePoll>, _poll_id: u64) -> Result<()> {
+    pub fn initialize_poll(
+        ctx: Context<InitializePoll>,
+        poll_id: u64,
+        description: String,
+        poll_start: u64,
+        poll_end: u64,
+    ) -> Result<()> {
+        let poll = &mut ctx.accounts.poll;
+        poll.poll_id = poll_id;
+        poll.description = description;
+        poll.poll_start = poll_start;
+        poll.poll_end = poll_end;
+        poll.candidate_amount = 0;
+
         Ok(())
     }
 }
